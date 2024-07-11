ddc08fea21f6e447dfa9f905951e8c8cc1781d5c
Rafael Treiber <rafaeltreiber@yahoo.com.br>
2024-07-11 18:14:19 -0300

    first setups

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { Button } from './components/ui/button'
+import './global.css'
 
 export function App() {
 
   return (
-    <h1>Treiber</h1>
+    <div className='flex justify-center items-center h-screen'>
+      <Button>Enviar</Button>
+    </div>
   )
 }
 
