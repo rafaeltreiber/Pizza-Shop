5077535d22a7dd5eafd93259d71f519835b2eb1b
Rafael Treiber <rafaeltreiber@yahoo.com.br>
2024-07-14 12:21:17 -0300

    initial setup done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import { Button } from './components/ui/button'
+import { RouterProvider } from 'react-router-dom'
 import './global.css'
+import { router } from './Pages/routes'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 
 export function App() {
 
   return (
-    <div className='flex justify-center items-center h-screen'>
-      <Button>Enviar</Button>
-    </div>
+    <HelmetProvider>
+      <Helmet titleTemplate='%s | pizza.shop' />
+      <RouterProvider router={router} />
+    </HelmetProvider>
   )
 }
 
---
+++ src/Pages/routes.tsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import { Dashboard } from "./app/dashboard";
+import { SignIn } from "./auth/sign-in";
+import { AppLayout } from "./_layouts/app";
+import { AuthLayout } from "./_layouts/auth";
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLayout />,
+    children: [{ path: '/', element: <Dashboard /> },]
+  },
+  {
+    path: '/',
+    element: <AuthLayout />,
+    children: [{ path: '/sign-in', element: <SignIn /> },]
+  },
+])
