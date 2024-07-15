e678bedf24a57b94330a93143110cde41f81152b
Rafael Treiber <rafaeltreiber@yahoo.com.br>
2024-07-15 19:56:25 -0300

    add react hook form and toast libraries

--- src/Pages/auth/sign-in.tsx
+++ src/Pages/auth/sign-in.tsx
@@ -2,8 +2,36 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Helmet } from 'react-helmet-async'
+import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
+import { z } from 'zod'
+
+const signInForm = z.object({
+  email: z.string().email()
+})
+
+type SignInForm = z.infer<typeof signInForm>
 
 export function SignIn() {
+  const { register, handleSubmit, formState: { isSubmitting } } = useForm<SignInForm>()
+
+  async function handleSignIn(data: SignInForm) {
+    try {
+      console.log(data)
+      await new Promise(resolve => setTimeout(resolve, 2000))
+
+      toast.success('Enviamos um link de autenticação para o seu e-mail.', {
+        action: {
+          label: 'Reenviar',
+          onClick: () => handleSignIn(data)
+        }
+      })
+    } catch (error) {
+
+    }
+
+  }
+
   return (
     <>
       <Helmet title='Login' />
@@ -14,14 +42,14 @@ export function SignIn() {
             <p className='text-sm text-muted-foreground'>Acompanhe suas vendas pelo painel do parceiro!</p>
           </div>
 
-          <form className='space-y-4'>
+          <form className='space-y-4' onSubmit={handleSubmit(handleSignIn)}>
             <div className='space-y-2'>
               <Label htmlFor='email'>Seu e-mail</Label>
-              <Input id='email' type='email' />
+              <Input id='email' type='email' {...register('email')} />
 
             </div>
 
-            <Button className='w-full' type='submit'>Acessar painel</Button>
+            <Button disabled={isSubmitting} className='w-full' type='submit'>Acessar painel</Button>
           </form>
 
         </div>
