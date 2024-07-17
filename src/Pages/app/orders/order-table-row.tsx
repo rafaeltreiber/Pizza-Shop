9a68b18a74a0a220085c47dffbfac78354bc73aa
Rafael Treiber <rafaeltreiber@yahoo.com.br>
2024-07-17 19:15:25 -0300

    create filters in orders page

---
+++ src/Pages/app/orders/order-table-row.tsx
@@ -0,0 +1,40 @@
+import { Button } from "@/components/ui/button";
+import { TableRow, TableCell } from "@/components/ui/table";
+import { Search, ArrowRight, X } from "lucide-react";
+
+export function OrderTableRow() {
+  return (
+    <TableRow>
+      <TableCell>
+        <Button variant="outline" size="xs">
+          <Search className="h-3 w-3" />
+          <span className="sr-only">Detalhes do pedido</span>
+        </Button>
+      </TableCell>
+      <TableCell className="font-mono text-xs font-medium">2g3j21g3jhg21j3j2</TableCell>
+      <TableCell className="text-muted-foreground">há 15 minutos</TableCell>
+      <TableCell>
+        <div className="flex items-center gap-2">
+          <span className="rounded-full h-2 w-2 bg-slate-400"></span>
+          <span className="font-medium text-muted-foreground">Pendente</span>
+        </div>
+      </TableCell>
+      <TableCell className="font-medium">Rafael Gonçalves Treiber</TableCell>
+      <TableCell className="font-medium">
+        R$ 149,90
+      </TableCell>
+      <TableCell>
+        <Button variant="outline" size="xs">
+          <ArrowRight className="h-3 w-3 mr-2" />
+          Aprovar
+        </Button>
+      </TableCell>
+      <TableCell>
+        <Button variant="ghost" size="xs">
+          <X className="h-3 w-3 mr-2" />
+          Cancelar
+        </Button>
+      </TableCell>
+    </TableRow>
+  )
+}
