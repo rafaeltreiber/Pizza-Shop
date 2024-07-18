95bef338d4475b4c3574f49463e376fdbe6abc62
Rafael Treiber <rafaeltreiber@yahoo.com.br>
2024-07-18 20:04:31 -0300

    add dashboard cards

--- src/Pages/routes.tsx
+++ src/Pages/routes.tsx
@@ -1,10 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
-import { Dashboard } from "./app/dashboard";
 import { SignIn } from "./auth/sign-in";
 import { AppLayout } from "./_layouts/app";
 import { AuthLayout } from "./_layouts/auth";
 import { SignUp } from "./auth/sign-up";
 import { Orders } from "./app/orders/orders";
+import { Dashboard } from "./app/dashboard/dashboard";
 
 export const router = createBrowserRouter([
   {
