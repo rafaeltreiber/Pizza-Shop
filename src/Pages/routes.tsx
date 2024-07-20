d39c370f126dc4a262f0b273219b1381ac5863f9
Rafael Treiber <rafaeltreiber@yahoo.com.br>
2024-07-20 11:08:59 -0300

    add not found page

--- src/Pages/routes.tsx
+++ src/Pages/routes.tsx
@@ -5,11 +5,13 @@ import { AuthLayout } from "./_layouts/auth";
 import { SignUp } from "./auth/sign-up";
 import { Orders } from "./app/orders/orders";
 import { Dashboard } from "./app/dashboard/dashboard";
+import { NotFound } from "./404";
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
+    errorElement: <NotFound />,
     children: [
       { path: '/', element: <Dashboard /> },
       { path: '/orders', element: <Orders /> },
