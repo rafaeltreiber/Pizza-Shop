a7410f40e8e5ad2e2138ff6eb72848583a76a234
Rafael Treiber <rafaeltreiber@yahoo.com.br>
2024-07-20 22:28:21 -0300

    setup signIn authentication

--- src/Pages/auth/sign-in.tsx
+++ src/Pages/auth/sign-in.tsx
@@ -1,6 +1,8 @@
+import { signIn } from '@/api/sign-in'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import { useMutation } from '@tanstack/react-query'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
@@ -16,10 +18,13 @@ type SignInForm = z.infer<typeof signInForm>
 export function SignIn() {
   const { register, handleSubmit, formState: { isSubmitting } } = useForm<SignInForm>()
 
+  const { mutateAsync: authenticate } = useMutation({
+    mutationFn: signIn
+  })
+
   async function handleSignIn(data: SignInForm) {
     try {
-      console.log(data)
-      await new Promise(resolve => setTimeout(resolve, 2000))
+      await authenticate({ email: data.email })
 
       toast.success('Enviamos um link de autenticação para o seu e-mail.', {
         action: {
