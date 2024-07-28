936081754759dee67901da859515d3b8400a8c11
Rafael Treiber <rafaeltreiber@yahoo.com.br>
2024-07-28 20:30:56 -0300

    add loading state to orders and details

--- src/Pages/app/orders/order-details.tsx
+++ src/Pages/app/orders/order-details.tsx
@@ -6,6 +6,7 @@ import { DialogTitle } from "@radix-ui/react-dialog";
 import { useQuery } from "@tanstack/react-query";
 import { formatDistanceToNow } from "date-fns";
 import { ptBR } from "date-fns/locale";
+import { OrderDetailsSkeleton } from "./order-details-skeleton";
 
 export interface OrderDetailsProps {
   orderId: string
@@ -28,7 +29,7 @@ export function OrderDetails({ orderId, open }: OrderDetailsProps) {
         <DialogDescription>Detalhes do pedido</DialogDescription>
       </DialogHeader>
 
-      {order && (
+      {order ? (
         <div className="space-y-6">
           <Table>
             <TableBody>
@@ -97,6 +98,8 @@ export function OrderDetails({ orderId, open }: OrderDetailsProps) {
             </TableFooter>
           </Table>
         </div>
+      ) : (
+        <OrderDetailsSkeleton />
       )}
     </DialogContent>
   )
